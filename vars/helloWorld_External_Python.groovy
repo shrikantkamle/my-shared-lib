9956d001e98af0b1fa6a3cff647d12a92fcd8420
shrikant <kamle.shrikant@gmail.com>
2024-02-07 18:41:18 +0530

    using sharelib resources python file updated

--- vars/helloWorld_External_Python.groovy
+++ vars/helloWorld_External_Python.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]){
     loadLinuxScript(name: "hello-world.py" )
-    sh "./hello-world.py ${config.name} ${config.dayofWeek}" 
+    sh "python hello-world.py ${config.name} ${config.dayofWeek}" 
     }
