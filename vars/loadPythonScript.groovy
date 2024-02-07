9a7c4f3ad814f8fb008f4a2ac3be6e3003451985
shrikant <kamle.shrikant@gmail.com>
2024-02-07 19:22:50 +0530

    updated python file

--- vars/loadPythonScript.groovy
+++ vars/loadPythonScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]){
-    def scriptContent = libraryResource "linux/${config.name}"
+    def scriptContent = libraryResource "python/${config.name}"
     writeFile file: "${config.name}", text: scriptContent
     sh "chmod a+x ./${config.name}"
     
