c48570737764d09a624988d64e8c686fb683ed1c
shrikant <kamle.shrikant@gmail.com>
2024-02-07 18:59:45 +0530

    added cat command

--- vars/helloWorld_External_Python.groovy
+++ vars/helloWorld_External_Python.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]){
     loadPythonScript(name: "hello-world.py" )
+    sh "cat  hello-world.py"
     sh "python -m hello-world.py ${config.name} ${config.dayofWeek}" 
     }
