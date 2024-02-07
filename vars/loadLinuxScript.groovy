4c73d18c6d9b8df7dc2459ea28414c6f1b69c105
shrikant <kamle.shrikant@gmail.com>
2024-02-07 18:27:50 +0530

    using sharelib resources in jenkins load linix file modified

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]){
     def scriptContent = libraryResource "linux/${config.name}"
-    writeFile file: ${config.name} text: scriptContent
+    writeFile file: "${config.name}", text: scriptContent
     sh "chmod a+x ./${config.name}"
     sh "./hello-world.sh ${config.name} ${config.dayofWeek}" 
     }
